5fe706de0d44b547b9853d2b55370f74a997da4d
Ramon <ramonmartinezcordero@gmail.com>
2023-08-03 12:05:55 -0400

    initial config

---
+++ README.md
@@ -0,0 +1,16 @@
+# Teslo
+
+```bash
+yarn install
+```
+
+## Configurar las ENV
+ 
+## Docker UP
+```bash
+docker-compose up -d
+```
+
+```bash
+yarn start:dev
+```
---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+@Module({
+  imports: [
+    // Variables de entorno
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT, 5432),
+      database: process.env.POSTGRES_DB,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
